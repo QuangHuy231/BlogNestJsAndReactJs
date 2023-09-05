c93544dd6c49d21494cec74200722fb8083aa9d8
Quang Huy <nguyenquanghuya3kd@gmail.com>
2023-09-05 15:01:04 +0700

    add folder in Cloudinary

--- backend/src/cloudinary/cloudinary.service.ts
+++ backend/src/cloudinary/cloudinary.service.ts
@@ -10,6 +10,9 @@ export class CloudinaryService {
   uploadFile(file: Express.Multer.File): Promise<CloudinaryResponse> {
     return new Promise<CloudinaryResponse>((resolve, reject) => {
       const uploadStream = cloudinary.uploader.upload_stream(
+        {
+          folder: 'blog',
+        },
         (error, result) => {
           if (error) return reject(error);
           resolve(result);
